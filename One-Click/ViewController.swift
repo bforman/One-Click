e0d8fa5208308836dbae5a34b25b530c524f7b40
Ben <bforman@users.noreply.github.com>
2016-02-02 22:17:48 -0500

    Connection to RBL Shield over BLE accomplished

--- One-Click/ViewController.swift
+++ One-Click/ViewController.swift
@@ -86,7 +86,6 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     @IBOutlet weak var messageField: UILabel!
 
